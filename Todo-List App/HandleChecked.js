437c3011f7cd8b94b8d450f440a980cf996bd3ad
Sujitha Ramanathan <164135691+CreativeAzign@users.noreply.github.com>
2024-04-07 02:32:20 +0530

    Delete Todo-List App directory

--- Todo-List App/HandleChecked.js
+++
@@ -1,9 +0,0 @@
-// Function to handle checkbox change event
-export function handleCheckboxChange(event) {
-  if (event.target.matches('input[type="checkbox"]')) {
-    const itemHolder = event.target.closest(".item-holder-class");
-    if (itemHolder) {
-      itemHolder.classList.toggle("checked", event.target.checked);
-    }
-  }
-}
